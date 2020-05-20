ea5c8260cc52d039670a506eb7ed8f4d81854683
GangWoon <gangwoon2018@gmail.com>
2020-05-20 21:10:28 +0900

    [#9] feat: 검색창 그림자 적용
    Custom View Class 생성
    @IBDesignable 적용
    shadow 값 설정

---
+++ iOS/Airbnb/Airbnb/StyleView/SearchTextField.swift
@@ -0,0 +1,22 @@
+//
+//  SearchTextField.swift
+//  Airbnb
+//
+//  Created by Cloud on 2020/05/20.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+final class SearchTextField: UITextField {
+    
+    @IBInspectable var shadowOpacity: Float {
+        get { layer.shadowOpacity }
+        set { layer.shadowOpacity = newValue }
+    }
+    @IBInspectable var shadowOffset: CGSize {
+        get { layer.shadowOffset }
+        set { layer.shadowOffset = newValue }
+    }
+}
