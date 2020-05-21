e785af03b5853b05beb8dc4990aef90c70b8be1c
GangWoon <gangwoon2018@gmail.com>
2020-05-21 14:27:29 +0900

    [#9] feat: 숙소 커스텀 셀 생성
    cell identifier을 static으로  생성했습니다.

---
+++ iOS/Airbnb/Airbnb/Accommodations/TableView/AccommodationsCell.swift
@@ -0,0 +1,15 @@
+//
+//  AccommodationsCell.swift
+//  Airbnb
+//
+//  Created by Cloud on 2020/05/21.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+final class AccommodationsCell: UITableViewCell {
+    
+    // MARK: - Properties
+    static let identifier: String = "AccommodationsCell"
+}
