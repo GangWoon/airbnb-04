54848e2a153cd01578a43d74241fca077dc71edf
GangWoon <gangwoon2018@gmail.com>
2020-05-21 14:29:12 +0900

    [#9] feat: ViewController -> AccommodationsViewController
    ViewController을 AccommodationsViewController 로 교체했습니다.

---
+++ iOS/Airbnb/Airbnb/Accommodations/AccommodationsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AccommodationsViewController.swift
+//  Airbnb
+//
+//  Created by Cloud on 2020/05/21.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+final class AccommodationsViewController: UIViewController {
+    
+}
