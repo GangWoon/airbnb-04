d9295e72c296c12bef7df6356295882f2ba0b00a
TTOzzi <devttozzi@gmail.com>
2020-05-21 16:02:54 +0900

    [#9] feat: PageConrol 연동
    스크롤 뷰와 페이지 컨트롤 연결
    스크롤 뷰가 스크롤 되고 난 뒤 화면에 맞게 페이지 표시

--- iOS/Airbnb/Airbnb/Accommodations/TableView/AccommodationsCell.swift
+++ iOS/Airbnb/Airbnb/Accommodations/TableView/AccommodationsCell.swift
@@ -10,6 +10,34 @@ import UIKit
 
 final class AccommodationsCell: UITableViewCell {
     
+    // MARK: - IBOutlets
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.delegate = self
+        }
+    }
+    @IBOutlet weak var pageControl: UIPageControl! {
+        didSet {
+            configurePageControl()
+        }
+    }
+    
     // MARK: - Properties
     static let identifier: String = "AccommodationsCell"
+    
+    // MARK: - Methods
+    private func configurePageControl() {
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = 3
+    }
+}
+
+// MARK: - UIScrollView
+// MARK: Delegate
+extension AccommodationsCell: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = round(scrollView.contentOffset.x / scrollView.frame.width)
+        pageControl.currentPage = Int(page)
+    }
 }
