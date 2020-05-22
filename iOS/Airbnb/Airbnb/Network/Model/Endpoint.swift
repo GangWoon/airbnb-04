0c65049aa90dca0dc00f4d3a7f04446a76ea1101
TTOzzi <devttozzi@gmail.com>
2020-05-22 16:27:17 +0900

    [#11] feat: Endpoint 생성

---
+++ iOS/Airbnb/Airbnb/Network/Model/Endpoint.swift
@@ -0,0 +1,39 @@
+//
+//  Endpoint.swift
+//  Airbnb
+//
+//  Created by TTOzzi on 2020/05/22.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import Foundation
+
+protocol RequestPorviding {
+    
+    var url: URL? { get }
+}
+
+struct Endpoint: RequestPorviding {
+    
+    enum Path: CustomStringConvertible {
+        case main
+    
+        var description: String {
+            switch self {
+            case .main:
+                return "/main"
+            }
+        }
+    }
+    
+    private let baseURL: String = "api/airbnb"
+    let path: Path
+    var url: URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = baseURL
+        components.path = path.description
+        
+        return components.url
+    }
+}
