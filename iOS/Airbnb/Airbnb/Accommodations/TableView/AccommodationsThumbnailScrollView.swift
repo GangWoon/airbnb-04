bd2ade91778d1eff97ab49d7e87a178ed5a36990
GangWoon <gangwoon2018@gmail.com>
2020-05-25 18:56:39 +0900

    [#11] feat: Thumbnail ScrollView 커스텀 클래스 생성

---
+++ iOS/Airbnb/Airbnb/Accommodations/TableView/AccommodationsThumbnailScrollView.swift
@@ -0,0 +1,14 @@
+//
+//  AccommodationsThumbnailScrollView.swift
+//  Airbnb
+//
+//  Created by Cloud on 2020/05/25.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+final class AccommodationsThumbnailScrollView: UIScrollView {
+    
+    @IBOutlet weak var stackView: UIStackView!
+}
