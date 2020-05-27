97779e713b2eec05d48b2274e2522b8398da23b7
TTOzzi <devttozzi@gmail.com>
2020-05-27 21:04:08 +0900

    [#13] feat: 콜렉션 뷰 헤더 UI 구현

--- iOS/Airbnb/Airbnb/Accommodations/ConditionSetting/DatePicker/Paging/Calendar/CalendarHeaderView.swift
+++ iOS/Airbnb/Airbnb/Accommodations/ConditionSetting/DatePicker/Paging/Calendar/CalendarHeaderView.swift
@@ -9,5 +9,32 @@
 import UIKit
 
 class CalendarHeaderView: UICollectionReusableView {
+    
+    // MARK: - Properties
     static let identifier: String = "CalendarHeaderView"
+    private var monthLabel: UILabel!
+    
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    // MARK: - Methods
+    private func configure() {
+        backgroundColor = .systemBackground
+        monthLabel = UILabel()
+        monthLabel.text = "Month"
+        monthLabel.font = .boldSystemFont(ofSize: 14)
+        addSubview(monthLabel)
+        monthLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(16)
+        }
+    }
 }
