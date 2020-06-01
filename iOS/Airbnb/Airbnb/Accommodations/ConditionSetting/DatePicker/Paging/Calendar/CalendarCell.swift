c53f1dcdfa31ae25ba34011b1694359387776149
TTOzzi <devttozzi@gmail.com>
2020-06-01 10:59:19 +0900

    [#25] feat: 날짜 버튼 클릭 시 배경색 변화 구현

--- iOS/Airbnb/Airbnb/Accommodations/ConditionSetting/DatePicker/Paging/Calendar/CalendarCell.swift
+++ iOS/Airbnb/Airbnb/Accommodations/ConditionSetting/DatePicker/Paging/Calendar/CalendarCell.swift
@@ -28,17 +28,34 @@ final class CalendarCell: UICollectionViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
         dayButton.setTitle("", for: .normal)
+        dayButton.isSelected = false
+        contentView.layer.cornerRadius = 0
+        contentView.backgroundColor = .systemBackground
     }
     
+    // MARK: - Methods
+    
+    @objc func dayButtonTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        contentView.layer.cornerRadius = contentView.frame.height / 2
+        contentView.backgroundColor = sender.isSelected ? .systemGray : .systemBackground
+    }
+    
+    // MARK: Configure
     private func configure() {
         backgroundColor = .systemBackground
+        configureDayButton()
+    }
+
+    private func configureDayButton() {
         dayButton = UIButton()
         dayButton.setTitleColor(.black, for: .normal)
         dayButton.titleLabel?.textAlignment = .center
         dayButton.titleLabel?.font = .boldSystemFont(ofSize: 11)
+        dayButton.addTarget(self, action: #selector(dayButtonTapped(_:)), for: .touchUpInside)
         addSubview(dayButton)
     }
-
+    
     // MARK: Constraints
     private func makeConstraints() {
         dayButton.snp.makeConstraints { make in
