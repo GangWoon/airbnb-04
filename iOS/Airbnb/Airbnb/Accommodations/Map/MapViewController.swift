a8324c64e90b06985d9ef416adf051d2e442aa77
GangWoon <gangwoon2018@gmail.com>
2020-06-05 11:26:54 +0900

    [#53] feat: Create MapViewController

---
+++ iOS/Airbnb/Airbnb/Accommodations/Map/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  Airbnb
+//
+//  Created by Cloud on 2020/06/05.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
